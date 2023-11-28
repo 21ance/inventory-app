ceeeab955937dd9f93b94d4b976a3f7372a7284b
Lance Lopez <lancelopez46@gmail.com>
2023-11-28 14:39:10 +0800

    add item create

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,13 +1,23 @@
 const Item = require("../models/item");
 const Category = require("../models/category");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 exports.item_list = asyncHandler(async (req, res, next) => {
-	const allItems = await Item.find({}).sort({ name: 1 }).exec();
+	const allItems = await Item.find().sort({ name: 1 }).exec();
+	const allCategory = await Category.find().exec();
 
 	res.render("item_list", {
 		title: "Items",
+		item: {
+			name: "",
+			description: "",
+			category: "",
+			price: "",
+			stock: "",
+		},
 		item_list: allItems,
+		category_list: allCategory,
 	});
 });
 
@@ -27,9 +37,61 @@ exports.item_detail = asyncHandler(async (req, res, next) => {
 	});
 });
 
-exports.item_create_post = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED: Item Create POST");
-});
+exports.item_create_post = [
+	body("name", "item name must contain 10 - 100 characters")
+		.trim()
+		.isLength({ min: 10, max: 100 })
+		.escape(),
+	body("description", "Description must not exceed 200 characters")
+		.trim()
+		.isLength({ max: 200 })
+		.escape(),
+	body("category", "Category must not be empty")
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+	body("price", "Invalid amount")
+		.trim()
+		.isLength({ min: 0, max: 8 })
+		.escape(),
+	body("stock", "Stock limit is 0 - 999")
+		.trim()
+		.isLength({ min: 0, max: 3 })
+		.escape(),
+	// to add image
+
+	asyncHandler(async (req, res, next) => {
+		const allItems = await Item.find().exec();
+		const errors = validationResult(req);
+		const item = new Item({
+			category: req.body.category,
+			name: req.body.name,
+			description: req.body.description,
+			price: req.body.price,
+			stock: req.body.stock,
+		});
+		if (req.body.price === "") {
+			item.price = 0;
+		}
+		if (req.body.stock === "") {
+			item.stock = 0;
+		}
+		if (!errors.isEmpty()) {
+			console.log(errors.array());
+		} else {
+			const filterDuplicate = allItems.filter(
+				(i) => i.name.toLowerCase() === item.name.toLowerCase()
+			);
+			if (filterDuplicate.length === 0) {
+				await item.save();
+				res.redirect(item.url);
+			} else {
+				// to fix, currently just infinitely loads
+				console.log(`${item.name} already exists`);
+			}
+		}
+	}),
+];
 
 exports.item_delete_post = asyncHandler(async (req, res, next) => {
 	res.send("NOT IMPLEMENTED: Item Delete POST");
---
+++ public/javascripts/item.js
@@ -0,0 +1,40 @@
+modalForm.addEventListener("submit", (e) => {
+	const item = {
+		category: document.forms["modalForm"]["category"].value,
+		name: document.forms["modalForm"]["name"].value,
+		description: document.forms["modalForm"]["description"].value,
+		price: Number(document.forms["modalForm"]["price"].value),
+		stock: Number(document.forms["modalForm"]["stock"].value),
+	};
+
+	if (item.category == "select") {
+		document.querySelector("#category-error").textContent =
+			"Please select a category";
+		e.preventDefault();
+	}
+
+	if (item.name < 10 || item.name > 100) {
+		document.querySelector("#name-error").textContent =
+			"Name must contain 10 - 100 characters";
+		e.preventDefault();
+	}
+
+	if (item.description.length > 200) {
+		document.querySelector("#description-error").textContent =
+			"Description must not exceed 200 characters";
+		e.preventDefault();
+	}
+
+	if (item.price < 0 || item.price > 100000) {
+		document.querySelector("#price-error").textContent = "Invalid amount";
+		e.preventDefault();
+	}
+
+	if (item.stock < 0 || item.stock > 999) {
+		document.querySelector("#stock-error").textContent =
+			"Stock must be: 0 - 999";
+		e.preventDefault();
+	}
+
+	console.log(item);
+});
