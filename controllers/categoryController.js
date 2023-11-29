d022c7db306b6d42d16e0cec5d11415435184567
Lance Lopez <lancelopez46@gmail.com>
2023-11-29 20:03:34 +0800

    add category image upload > install multer

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,6 +3,27 @@ const Item = require("../models/item");
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 
+// multer middleware
+// https://www.youtube.com/watch?v=wIOpe8S2Mk8
+const path = require("path");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, "public/images/upload_category");
+	},
+	filename: (req, file, cb) => {
+		cb(null, Date.now() + path.extname(file.originalname));
+	},
+});
+
+const upload = multer({
+	storage: storage,
+	limits: {
+		fileSize: 1000000,
+	},
+});
+
 exports.category_list = asyncHandler(async (req, res, next) => {
 	const allCategory = await Category.find({}).sort({ name: 1 }).exec();
 
@@ -35,6 +56,8 @@ exports.category_detail = asyncHandler(async (req, res, next) => {
 });
 
 exports.category_create_post = [
+	upload.single("image_upload"),
+
 	body("name", "Category name must contain 3 - 100 characters")
 		.trim()
 		.isLength({ min: 3, max: 100 })
@@ -43,7 +66,6 @@ exports.category_create_post = [
 		.trim()
 		.isLength({ max: 200 })
 		.escape(),
-	// to add image
 
 	asyncHandler(async (req, res, next) => {
 		const allCategory = await Category.find().exec();
@@ -52,6 +74,9 @@ exports.category_create_post = [
 			name: req.body.name,
 			description: req.body.description,
 		});
+		if (req.file !== undefined) {
+			category.image = req.file.filename;
+		}
 		if (!errors.isEmpty()) {
 			console.log(errors.array());
 		} else {
@@ -82,6 +107,8 @@ exports.category_delete_post = asyncHandler(async (req, res, next) => {
 });
 
 exports.category_update_post = [
+	upload.single("image_upload"),
+
 	body("name", "Category name must contain 3 - 100 characters")
 		.trim()
 		.isLength({ min: 3, max: 100 })
@@ -93,27 +120,20 @@ exports.category_update_post = [
 	// to add image
 
 	asyncHandler(async (req, res, next) => {
-		const allCategory = await Category.find().exec();
 		const errors = validationResult(req);
 		const category = new Category({
 			name: req.body.name,
 			description: req.body.description,
 			_id: req.params.id,
 		});
+		if (req.file !== undefined) {
+			category.image = req.file.filename;
+		}
 		if (!errors.isEmpty()) {
 			console.log(errors.array());
 		} else {
-			const filterDuplicate = allCategory.filter(
-				(cat) => cat.name.toLowerCase() === category.name.toLowerCase()
-			);
-			if (filterDuplicate.length === 0) {
-				console.log(category);
-				await Category.findByIdAndUpdate(req.params.id, category);
-				res.redirect(category.url);
-			} else {
-				// to fix, currently just infinitely loads
-				console.log(`${category.name} already exists`);
-			}
+			await Category.findByIdAndUpdate(req.params.id, category);
+			res.redirect(category.url);
 		}
 	}),
 ];
